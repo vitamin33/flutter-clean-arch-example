e59794756c1b5838f2b055af0fd83c5ba293395c
Vitalii Serbyn <vivitseser@gmail.com>
2023-11-07 13:04:51 +0200

    feat: add utils and update pubspec

---
+++ flutter_cleanarch_example/lib/src/utils/constants/strings.dart
@@ -0,0 +1,11 @@
+// App
+const String appTitle = 'Flutter Clean Architecture';
+
+// Networking and APIs
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = 'd237f3cbd8c34c0d84ef02175ead521b';
+const String defaultSources = 'bbc-news, abc-news, al-jazeera-english';
+
+// Storage and Databases
+const String articlesTableName = 'articles_table';
+const String databaseName = 'app_database.db';
